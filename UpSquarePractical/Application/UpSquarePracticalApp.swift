bba1f528de873daf00a044e185f2ffb2fb4caf50
Gaurang Vyas <gaurang.vyas@solutionanalysts.com>
2021-07-26 17:01:27 +0530

    [init] Initial Setup

---
+++ UpSquarePractical/Application/UpSquarePracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpSquarePracticalApp.swift
+//  UpSquarePractical
+//
+//  Created by Gaurang Vyas on 26/07/21.
+//
+
+import SwiftUI
+
+@main
+struct UpSquarePracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Color.blue
+        }
+    }
+}
